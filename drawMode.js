05fa55df85407e813ff3b49fc0d3d267cd6af96b
cockroach54 <lsw0504@naver.com>
2017-07-31 10:35:46 +0900

    0731

--- drawMode.js
+++ drawMode.js
@@ -1,6 +1,6 @@
 
-var drawMode = 0; // [0] = landscape, [1] = still_life, [2] = portrait
-var isTalktive = true; // false = silent mode
+var drawMode; // [0] = landscape, [1] = still_life, [2] = portrait
+var isTalkative; // false = silent mode
 var task = 0;
 var taskText = new Array(3); // [0] = landscape, [1] = still_life, [2] = portrait
 var ticker_showed = true;
@@ -59,3 +59,22 @@ taskText[2] = ['안녕하세요. 그림을 그려주실래요?',
 '여기 빈 공간이 큰 것 같아요. 뭘 좀 그리는게 어떨까요?',
 '멋지네요! 이제 그림이 꽉 찬것 같아요.',
 '이제 그림에 어울리는 색을 추천해 주세요!']; //15
+
+//-----------silent mode
+var taskText_s = ['안녕하세요. 그림을 그려주실래요?',
+'야자나무를 그리는 중이군요!',
+'나머지 부분은 제가 그려볼게요!', //2 - setTimeout 5sec
+'헤헤, 맘에 드시나요?',
+// '그럼 다시 한 번 그려볼게요',
+'그럼 또 다른 야자나무를 하나 더 그려볼게요!', //5 - check for gen
+'헤헤, 맘에 드시나요?',
+'야자나무와 어울리는 코코넛을 그려볼게요!', //7 - check for vae
+'헤헤, 맘에 드시나요?', //8
+// '그럼 다시 한 번 그려볼게요',
+// '이번이 마지막이에요!',
+'빈 공간에 그림을 더 그려주세요!', //11 - pen twinkle 3 times
+'멋진 그림이네요!',
+'여기 빈 공간이 큰 것 같아요. 뭘 좀 그리는게 어떨까요?', //13 - alertWhiteSpace()
+'멋지네요! 이제 그림이 꽉 찬것 같아요.',
+'이제 그림에 어울리는 색을 추천해 주세요!']; //15 - brush twinkle 3 times]
+
